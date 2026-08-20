70f9194b332e1ae60a0fd695f7fabbd663d678d8
Wanderson Honorato <wanderson.honorato1998@gmail.com>
2026-08-20 14:24:36 -0300

    fix: corrigir requires

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const { createApp } = require('./app');
-const { env } = require('./config/env');
-const { logger } = require('./utils/logger');
+const { env } = require('./src/config/env');
+const { logger } = require('./src/utils/logger');
 
 const app = createApp();
 
 app.listen(env.port, () => {
   logger.info(`Servidor ouvindo na porta ${env.port}`);
   logger.info(`BASE_URL configurada como ${env.baseUrl}`);
-  logger.info(`Endpoint chamado pelo Flow da Suri: POST ${env.baseUrl}/api/case-tecnico-integracao-chatbot`,);
-  
+  logger.info(`Endpoint chamado pelo Flow da Suri: POST ${env.baseUrl}/api/integracaoAPI`);
+
   if (!env.suriApiBaseUrl || !env.suriApiToken) {
-    logger.warn("SURI_API_TOKEN",);
+    logger.warn(
+      'SURI_API_BASE_URL/SURI_API_TOKEN não configurados',
+    );
   }
 });
