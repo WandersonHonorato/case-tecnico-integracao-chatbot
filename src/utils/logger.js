e6bb4ed0f3fb6bcfade14ea8d1cd50a6448d90d5
Wanderson Honorato <wanderson.honorato1998@gmail.com>
2026-08-20 11:13:14 -0300

    feat: padronizar as mensagens que aparecem no terminal

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,15 +4,12 @@ function timestamp() {
 
 const logger = {
   info(message, meta) {
-    // eslint-disable-next-line no-console
     console.log(`[${timestamp()}] INFO  ${message}`, meta || '');
   },
   warn(message, meta) {
-    // eslint-disable-next-line no-console
     console.warn(`[${timestamp()}] WARN  ${message}`, meta || '');
   },
   error(message, meta) {
-    // eslint-disable-next-line no-console
     console.error(`[${timestamp()}] ERROR ${message}`, meta || '');
   },
 };
