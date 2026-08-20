65eb6dcac13a6f4715b6f01b3bbffd6d27612599
Wanderson Honorato <wanderson.honorato1998@gmail.com>
2026-08-20 02:54:47 -0300

    feat: definir a rota HTTP chamada pela ação de requisição da API do chatbot

---
+++ src/routes/integracaoApiroutes.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const { IntegracaoApiController } = require('../controllers/segundaVia.controller');
+const { apiKeyGuard } = require('../middleware/apiKey.middleware');
+
+/**
+ * @param {import('../services/boleto.repository').JsonBoletoRepository} repository
+ * @param {import('../services/suriApi.client').SuriApiClient} suriApiClient
+ */
+function integracaoApiRouter(repository, suriApiClient) {
+  const router = Router();
+
+  router.post('/integracaoAPI', apiKeyGuard, createIntegracaoApiController(repository, suriApiClient));
+
+  return router;
+}
+
+module.exports = { integracaoApiRouter };
